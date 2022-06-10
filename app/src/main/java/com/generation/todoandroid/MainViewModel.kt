51b22627b2444d98d54b54b4244c3a5e761006e3
Daniela Lima <sousa.danielalima@gmail.com>
2022-06-10 12:08:22 -0300

    implemented retrofit

---
+++ app/src/main/java/com/generation/todoandroid/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.generation.todoandroid
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.generation.todoandroid.api.Repository
+import com.generation.todoandroid.model.Categoria
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class MainViewModel : ViewModel() {
+
+    val repository = Repository()
+
+    private val _myCategoriaResponse =
+        MutableLiveData<Response<List<Categoria>>>()
+
+    val myCategoriaResponse: LiveData<Response<List<Categoria>>> =
+        _myCategoriaResponse
+
+    fun listCategoria(){
+        viewModelScope.launch {
+            _myCategoriaResponse.value = repository.listCategoria()
+        }
+
+    }
+}
